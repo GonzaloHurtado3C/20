77ecc7ee5b52716dc89f6b3e0b92d67e9cc4269b
GonzaloHurtado <165288768+GonzaloHurtado3C@users.noreply.github.com>
2025-03-31 10:01:14 -0300

    Add files via upload

---
+++ 17 des. app web/ejercicio6/08_blucles/script.js
@@ -0,0 +1,40 @@
+function buclewhile() { 
+    let count = 10;
+    let array = [];
+    while (count >= 1){
+        array.push(count);
+        count--;
+    }
+    alert(`los numeros son: ${array.join(" - ")}`);
+}
+
+
+function buclefor() {
+    let count =10;let 
+    array = [];
+    for(i = 10; i >= 1; i--){
+        array.push(i);
+        count--;
+    }
+    alert(`los numeros son: ${array.join(" - ")}`);
+}
+
+function contarNumeros(){
+let n = parseInt(prompt("Ingrese un numero"));
+let count = 1;
+let array = [];
+while (n <= 1){
+    array.push(n);
+    i++;
+}
+}
+
+function sumarNumeros() {
+    let suma = 0;
+    let array = [];
+    for(let i = 1; i <=5; i++){
+        suma += i;
+        array.push(i);
+    }
+    
+}
